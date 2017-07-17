3d2d9445f72e11994fd887c8b5d72d1ba407e532
Sippaprut <sippaprut@turkenterprisesinc.com>
2017-07-17 18:38:02 +0700

    learn about asyncData

---
+++ pages/obits/index.vue
@@ -0,0 +1,32 @@
+<template>
+	 <section class="container">
+		{{ title }}
+		<div class="obit-items" v-for="obit in obits">
+			{{ obit.email }}
+		</div>
+
+	 </section>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  head: {
+    title: 'Obit Page',
+    meta: [
+      { name: 'description', content: 'Obit page description' },
+      { name: 'og:img', content: 'https://faceboox.com' }
+    ]
+  },
+  asyncData ({ params }) {
+    return axios.get('https://randomuser.me/api/')
+    .then(({data}) => {
+      return { obits: data.results, title: 'Hello Async' }
+    })
+  },
+  data () {
+    return { title: 'Hello Nuxtjs' }
+  }
+}
+</script>
+
